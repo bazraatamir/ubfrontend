fe48fa3949036e32167c1f32fd16a98009886f93
bazraatamir <lhagvasuren.bazarragchaa89@gmail.com>
2025-03-07 13:44:38 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Neriinhool from "./modules/RestaurantOwner/components/neriinhool";
 import RestaurantPage from "./pages/RestaurantPage";
 import AuthPage from "./pages/authPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div>
-      <RestaurantPage></RestaurantPage>
+      <HomePage />
     </div>
   );
 };
