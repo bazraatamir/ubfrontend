ad7ca819e466108977587d9b8f8837c9cb59b91f
bazraatamir <lhagvasuren.bazarragchaa89@gmail.com>
2025-03-15 14:33:28 +0800

    fueatur structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-import Neriinhool from "./modules/RestaurantOwner/components/neriinhool";
 import RestaurantPage from "./pages/RestaurantPage";
 import AuthPage from "./pages/authPage";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<RestaurantPage />} />
-        <Route path="/Auth" element={<AuthPage />} />
-        <Route path="/Login" element={<LoginPage />} />
-        <Route path="/Signup" element={<SignupPage />} />
-        <Route path="/Home" element={<HomePage />} />
-      </Routes>
-    </>
-  );
+  return <RestaurantPage />;
 };
 
 export default App;
